caaa658f6315cfc9f79afd3b6705590066319ff3
torvalds <deandb2015@gmail.com>
2017-10-24 09:29:01 +0800

    Implement advanced login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   # before_save { self.email = email.downcase }
   before_save { email.downcase! }
   # VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -13,12 +14,33 @@ class User < ApplicationRecord
 
   has_secure_password
 
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  # 如果指定的令牌和摘要匹配, 返回
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  # 忘记用户
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
+
   class << self
     # 返回指定字符串的哈希摘要
     def digest(string)
       cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
         BCrypt::Engine.cost
       BCrypt::Password.create(string, cost: cost)
+    end
+
+    # 返回一个随机令牌
+    def new_token
+      SecureRandom.urlsafe_base64
     end
   end
 end
