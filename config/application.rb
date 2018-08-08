7c6b5c3b42094526948a1569b5342853ff4f6fe6
torvalds <deandb2015@gmail.com>
2018-08-09 00:31:45 +0800

    Modify application.rb

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,27 @@ module SampleApp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    # Add this config can affect Faker::Name.name # => "张三"
+    config.i18n.default_locale = :'zh-CN'
+
+    # Format time
+    config.time_zone = 'Beijing'
+    config.active_record.default_timezone = :local
+
+    # custom generate
+    config.generators do |g|
+      g.test_framework  false
+      g.assets false
+      g.helper false
+      g.jbuilder false
+    end
+
+    # Don't generate system test files.
+    config.generators.system_tests = nil
+
+    # autoload lib/*
+    config.eager_load_paths += %W[#{config.root}/lib]
+    config.autoload_paths << Rails.root.join('lib')
   end
 end
